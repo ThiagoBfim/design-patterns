5a799abb1764ba9b029902d2eee83976d1f70293
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-01-19 11:35:29 -0200

    Inclusão do documentação do Builder

--- src/main/java/br/org/project/design_patterns/creational/builder/TestBuilder.java
+++ src/main/java/br/org/project/design_patterns/creational/builder/TestBuilder.java
@@ -3,7 +3,22 @@ package br.org.project.design_patterns.creational.builder;
 import br.org.project.design_patterns.creational.builder.old.Computador;
 import br.org.project.design_patterns.creational.builder.pattern.ComputadorBuilder;
 
-
+/**
+ * Pattern:
+ *  Problema:   Há uma classe com varios atributos, a construção de uma nova instância está tornando o código pouco verboso,
+ *              sendo dificil de ler.
+ *  Solução:    Evoluir essa classe para utilizar o padrão builder.
+ *
+ *  Builder Pattern :
+ *      * O padrão Builder pode ser utilizado para tornar o código mais verboso
+ *      * Há varias formas de implementar o builder, ele também pode ser utilizado para forçar que a implementação contenha
+ *      todos os atributos obrigatórios, e possibilitar incluir os opicionais de forma mais limpa.
+ *
+ *  Note: Esse foi apenas um exemplo de possíveis implementações do padrão Builder.
+ *        O core do Java utiliza esse padrão no StringBuilder por exemplo.
+ *
+ * @see  java.lang.StringBuilder#append(String)
+ */
 public class TestBuilder {
 
     public static void main(String[] args) {
