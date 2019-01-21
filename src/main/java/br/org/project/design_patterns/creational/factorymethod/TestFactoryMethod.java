b93c1f9df1cf55ffd9488a6e208ee335e8e73c22
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-01-21 19:02:43 -0200

    Inclusão do comentario para o factory method

--- src/main/java/br/org/project/design_patterns/creational/factorymethod/TestFactoryMethod.java
+++ src/main/java/br/org/project/design_patterns/creational/factorymethod/TestFactoryMethod.java
@@ -4,6 +4,18 @@ import br.org.project.design_patterns.creational.factorymethod.creator.FactoryPi
 import br.org.project.design_patterns.creational.factorymethod.creator.concretecreator.BrasiliaFactoryPizza;
 import br.org.project.design_patterns.creational.factorymethod.creator.concretecreator.NYFactoryPizza;
 
+/**
+ * Pattern:
+ *  Problema:   Há uma fabrica de pizza, entretanto, essa fabrica está querendo crescer para outras localidades, e cada localidade
+ *              poderá ter pizzas de sabores diferentes.
+ *  Solução:    Criar uma Factory que é responsável por conter o conhecimento de como produz a pizza. E criar subclasses que
+ *              tem como objetivo fornecer as pizzas de cada fabrica especifica.
+ *
+ *  Factory Method Pattern :
+ *      * O padrão Factory Method pode ser utilizado quando não foi possível antecipar quais objetos a classe deverá criar.
+ *      * Quando as subclasses tem como objetivo defirnir quais objetos elas irão criar.
+ *      * Quando as subclasses tem como objetivo fornecer um objeto especifico que poderá ser utilizado pela superclsse.
+ */
 public class TestFactoryMethod {
 
     public static void main(String[] args) {
